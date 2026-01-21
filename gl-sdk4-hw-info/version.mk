e791036aac6bfb417b4fc0d4a50b59d28c2e7aa1
KUAPT <yanruiad@126.com>
2026-01-21 15:10:47 +0800

    fix: gl-sdk4-hw-info: make version apk-safe
    apk versions only allow '-' for the -rN revision; replace git findrev separators with '.' to avoid extra hyphens.

--- gl-sdk4-hw-info/version.mk
+++ gl-sdk4-hw-info/version.mk
@@ -11,7 +11,7 @@ define findrev
       if [ -n "$$1" ]; then
         secs="$$(($$1 % 86400))"; \
         yday="$$(date --utc --date="@$$1" "+%Y.%j")"; \
-        printf 'git-%s.%05d-%s' "$$yday" "$$secs" "$$2"; \
+        printf 'git.%s.%05d.%s' "$$yday" "$$secs" "$$2"; \
       else \
         echo "unknown"; \
       fi; \
